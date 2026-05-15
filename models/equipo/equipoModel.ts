fdd66c6118ac3b1215a9a092b988723a3c9f21d5
carlos_garcia <ga.carlos2003@gmail.com>
2026-05-15 17:08:08 -0600

    Fix - Correccion de tipo de dato en id de equipos

--- models/equipo/equipoModel.ts
+++ models/equipo/equipoModel.ts
@@ -1,7 +1,7 @@
 import { supabase } from "@/lib/supabase"
 
 export interface Equipo {
-    id?: Text
+    id?: string
     nombre_pais: string
     ranking_fifa: number
     id_federacion: number
@@ -15,11 +15,11 @@ export async function insertEquipo(equipo: Equipo) {
     return await supabase.from('equipo').insert(equipo)
 }
 
-export async function updateEquipo(id: Text, equipo: Equipo) {
+export async function updateEquipo(id: string, equipo: Equipo) {
     return await supabase.from('equipo').update(equipo).eq('id', id)
 }
 
-export async function deleteEquipo(id: Text) {
+export async function deleteEquipo(id: string) {
     return await supabase.from('equipo').delete().eq('id', id)
 }
 
